16fcf81161edfb19cf27c0374f1df3f0541a3d81
bigboss2021 <limavalmir2000@hotmail.com>
2021-08-31 12:51:46 -0300

    cadastrar e logar

---
+++ Desktop/Fundamentos-Web/Blog Pessoal/blogpessoal/src/app/model/Tema.ts
@@ -0,0 +1,12 @@
+
+
+import { Postagem } from "./Postagem";
+import { User } from "./User";
+
+
+export class Tema{
+
+    public id: number
+    public descriçao: string
+    public postagem: Postagem[]
+}
